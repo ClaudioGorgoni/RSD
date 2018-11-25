Correct a bug in v12 and speed-up the computation

GS-model/GS_multi.py
@@ -1,6 +1,6 @@
 import collections as collec
 import sys
-from multiprocessing import Pool, cpu_count
+from multiprocess import Pool, cpu_count
 
 import GS_sig_pi as gs
 import numpy as np
@@ -111,22 +111,22 @@ def xi_l(s, l, alpha_p, alpha_v, fz, sigma_FoG, F1, F2):
 
 	"""	
 	
-	if isinstance(s, (collec.Sequence, np.ndarray)):
+	#if isinstance(s, (collec.Sequence, np.ndarray)):
 
-		pool = Pool(cpu_count())
+	pool = Pool(cpu_count())
 
-		tmp  = [ (ss, l, alpha_p, alpha_v, fz, sigma_FoG, F1, F2) for ss in s]
+	tmp  = [ (ss, l, alpha_p, alpha_v, fz, sigma_FoG, F1, F2) for ss in s]
 		
-		res  = np.array( pool.map(mu_integral, tmp) )
+	res  = np.array( pool.map(mu_integral, tmp) )
 
-		#res = np.array( map(mu_integral, tmp) )
+	#res = np.array( map(mu_integral, tmp) )
 		
-		pool.close()
-		pool.join()
+	pool.close()
+	pool.join()
 
-	else:
+	#else:
 
-		res = mu_integral((s, l, alpha_p, alpha_v, fz, sigma_FoG, F1, F2))
+	#	res = mu_integral((s, l, alpha_p, alpha_v, fz, sigma_FoG, F1, F2))
 
 	return res
 
@@ -137,29 +137,29 @@ def xi_02(s, alpha_p, alpha_v, fz, sigma_FoG, F1, F2):
 
 	"""	
 	
-	if isinstance(s, (collec.Sequence, np.ndarray)):
+	#if isinstance(s, (collec.Sequence, np.ndarray)):
 	
-		tmp =  [ (ss, 0, alpha_p, alpha_v, fz, sigma_FoG, F1, F2) for ss in s]
-		tmp += [ (ss, 2, alpha_p, alpha_v, fz, sigma_FoG, F1, F2) for ss in s]
+	tmp =  [ (ss, 0, alpha_p, alpha_v, fz, sigma_FoG, F1, F2) for ss in s]
+	tmp += [ (ss, 2, alpha_p, alpha_v, fz, sigma_FoG, F1, F2) for ss in s]
 		
-		pool = Pool(cpu_count())
+	pool = Pool(cpu_count())
 			
-		res  = np.array( pool.map( mu_integral, tmp) )
+	res  = np.array( pool.map( mu_integral, tmp) )
 			
-		pool.close()
-		pool.join()
+	pool.close()
+	pool.join()
 	
-	else:
+	#else:
 	
-		tmp  =  [ (s, 0, alpha_p, alpha_v, fz, sigma_FoG, F1, F2) ]
-		tmp  += [ (s, 2, alpha_p, alpha_v, fz, sigma_FoG, F1, F2) ]
+	#	tmp  =  [ (s, 0, alpha_p, alpha_v, fz, sigma_FoG, F1, F2) ]
+	#	tmp  += [ (s, 2, alpha_p, alpha_v, fz, sigma_FoG, F1, F2) ]
 		
-		pool = Pool()
+	#	pool = Pool()
 			
-		res  = np.array( pool.map( mu_integral, tmp) )
+	#	res  = np.array( pool.map( mu_integral, tmp) )
 			
-		pool.close()
-		pool.join()
+	#	pool.close()
+	#	pool.join()
 		
 	return res
 
@@ -170,30 +170,30 @@ def xi_024(s, alpha_p, alpha_v, fz, sigma_FoG, F1, F2):
 
 	"""
 
-	if isinstance(s, (collec.Sequence, np.ndarray)):
+	#if isinstance(s, (collec.Sequence, np.ndarray)):
 
-		tmp =  [(ss, 0, alpha_p, alpha_v, fz, sigma_FoG, F1, F2) for ss in s]
-		tmp += [(ss, 2, alpha_p, alpha_v, fz, sigma_FoG, F1, F2) for ss in s]
-		tmp += [(ss, 4, alpha_p, alpha_v, fz, sigma_FoG, F1, F2) for ss in s]
+	tmp =  [(ss, 0, alpha_p, alpha_v, fz, sigma_FoG, F1, F2) for ss in s]
+	tmp += [(ss, 2, alpha_p, alpha_v, fz, sigma_FoG, F1, F2) for ss in s]
+	tmp += [(ss, 4, alpha_p, alpha_v, fz, sigma_FoG, F1, F2) for ss in s]
 
-		pool = Pool(cpu_count())
+	pool = Pool(cpu_count())
 
-		res = np.array( pool.map(mu_integral, tmp) )
+	res = np.array( pool.map(mu_integral, tmp) )
 
-		pool.close()
-		pool.join()
+	pool.close()
+	pool.join()
 
-	else:
+	#else:
 
-		tmp =  [(s, 0, alpha_p, alpha_v, fz, sigma_FoG, F1, F2)]
-		tmp += [(s, 2, alpha_p, alpha_v, fz, sigma_FoG, F1, F2)]
-		tmp += [(s, 4, alpha_p, alpha_v, fz, sigma_FoG, F1, F2)]
+	#	tmp =  [(s, 0, alpha_p, alpha_v, fz, sigma_FoG, F1, F2)]
+	#	tmp += [(s, 2, alpha_p, alpha_v, fz, sigma_FoG, F1, F2)]
+	#	tmp += [(s, 4, alpha_p, alpha_v, fz, sigma_FoG, F1, F2)]
 
-		pool = Pool()
+	#	pool = Pool()
 
-		res = np.array( pool.map(mu_integral, tmp) )
+	#	res = np.array( pool.map(mu_integral, tmp) )
 
-		pool.close()
-		pool.join()
+	#	pool.close()
+	#	pool.join()
 
 	return res

GS-model/GS_sig_pi.py
@@ -18,18 +18,33 @@ min_r = min(r)
 max_r = max(r)
 
 sqrt2pi = np.sqrt( 2. * np.pi )
+myeps   = 1.e-12
 
+# THE CLPT OUTPUT
+tmp_f1  = np.loadtxt(xi_file, unpack=True)
+tmp_xi  = np.array( [tmp_f1[2], tmp_f1[3], tmp_f1[4], tmp_f1[5], tmp_f1[6], tmp_f1[7]] )
 
-def get_r(): return r
+tmp_f2  = np.loadtxt(v12_file, unpack=True)
+tmp_v12 = np.array( [tmp_f2[2], tmp_f2[3], tmp_f2[4], tmp_f2[5], tmp_f2[6]] )
 
+tmp_f3  = np.loadtxt(s12_file, usecols=(0, 1, 2, 3, 4), unpack=True)
+tmp_sp  = np.array( [tmp_f3[1], tmp_f3[2], tmp_f3[3], tmp_f3[4]] )
+
+tmp_f4  = np.loadtxt(s12_file, usecols=(0, 5, 6, 7, 8), unpack=True)
+tmp_sv  = np.array( [tmp_f4[1], tmp_f4[2], tmp_f4[3], tmp_f4[4]] )
+
+#-------------------------------------------------------------------
+
+def get_r(): 
+	return r
 
 
 def xi_r(f10, f01, f20, f11, f02):
 	"""read xi from file"""
+	
+	ff  = np.array( [1., f10, f01, f20, f11, f02] )
 
-	tmp = np.loadtxt(xi_file, unpack=True)
-
-	val = tmp[2] + f10 * tmp[3] + f01 * tmp[4] + f20 * tmp[5] + f11 * tmp[6] + f02 * tmp[7]
+	val = np.dot(ff, tmp_xi)
 
 	return np.insert(val, 0, val[0] * 1.5)
 
@@ -37,10 +52,10 @@ def xi_r(f10, f01, f20, f11, f02):
 
 def v12(f10, f01, f20, f11, f02):
 	"""read v12 from file"""
+	
+	ff  = np.array( [1., f10, f01, f20, f11] )
 
-	tmp = np.loadtxt(v12_file, unpack=True)
-
-	val = tmp[1] + f10 * tmp[2] + f01 * tmp[3] + f20 * tmp[4] + f11 * tmp[5] + f02 * tmp[6]
+	val = np.dot(ff, tmp_v12)
 
 	val = np.insert(val, 0, 0.)
 
@@ -55,10 +70,10 @@ def s12_par(f10, f01, f20, f11, f02):
 	read s12 from file
 	- parallel component
 	"""
+	
+	ff  = np.array( [1., f10, f01, f20] )
 
-	tmp = np.loadtxt(s12_file, usecols=(0, 1, 2, 3, 4), unpack=True)
-
-	val = tmp[1] + f10 * tmp[2] + f01 * tmp[3] + f20 * tmp[4]
+	val = np.dot(ff, tmp_sp)
 
 	val = np.insert(val, 0, val[0])
 
@@ -73,10 +88,10 @@ def s12_per(f10, f01, f20, f11, f02):
 	read s12 from file
 	- perpendicular component
 	"""
+	
+	ff  = np.array( [1., f10, f01, f20] )
 
-	tmp = np.loadtxt(s12_file, usecols=(0, 5, 6, 7, 8), unpack=True)
-
-	val = tmp[1] + f10 * tmp[2] + f01 * tmp[3] + f20 * tmp[4]
+	val = np.dot(ff, tmp_sv)
 
 	val = np.insert(val, 0, val[0])
 

GS-model/mcmc.py
@@ -19,6 +19,9 @@ if config.get('input', 'data_xi4') != '':
 	N_multi.append(4)
 N_multi.sort()
 
+cond0 = 0 in N_multi
+cond2 = 2 in N_multi
+cond4 = 4 in N_multi
 
 peak_background = config.getboolean('mcmc', 'peak_background')
 ndim = 0
@@ -133,28 +136,26 @@ def lnlike(theta):
 	res     : (float) the log-likelihood
 	"""
 	
-	# if peak_background:
-	#  	alpha_p, alpha_v, fz, F1, sigma_FoG     = theta
-	#  	F2 = Lagrangian_bias2(F1)
-	#
-	# else:
-	#  	alpha_p, alpha_v, fz, F1, F2, sigma_FoG = theta
-	#
-	#
-	# if 0 in N_multi and 2 in N_multi:
-	# 	multi_model = xi_02(s, alpha_p, alpha_v, fz, sigma_FoG, F1, F2)
-	#
-	# elif 0 in N_multi and 2 in N_multi and 4 in N_multi:
-	# 	multi_model = xi_024(s, alpha_p, alpha_v, fz, sigma_FoG, F1, F2)
-	#
-	# else:
-	# 	multi_model = np.hstack([ xi_l(s, ll, alpha_p, alpha_v, fz, sigma_FoG, F1, F2) for ll in N_multi ])
-	#
-	#
-	# tmp = data - multi_model
-	#
-	# return - 0.5 * np.dot(tmp, np.dot(icov, tmp))
-	return 10.
+	if peak_background:
+		alpha_p, alpha_v, fz, F1, sigma_FoG     = theta
+		F2 = Lagrangian_bias2(F1)
+	
+	else:
+		alpha_p, alpha_v, fz, F1, F2, sigma_FoG = theta
+		
+	
+	if cond0 and cond2 and not cond4:
+		multi_model = xi_02(s, alpha_p, alpha_v, fz, sigma_FoG, F1, F2)
+	
+	elif cond0 and cond2 and cond4:
+		multi_model = xi_024(s, alpha_p, alpha_v, fz, sigma_FoG, F1, F2)
+	
+	else:
+		multi_model = np.hstack([ xi_l(s, ll, alpha_p, alpha_v, fz, sigma_FoG, F1, F2) for ll in N_multi ])
+
+	tmp = data - multi_model
+	
+	return - 0.5 * np.dot(tmp, np.dot(icov, tmp))
 
 
 def lnprob(theta):
@@ -169,7 +170,7 @@ def lnprob(theta):
 		
 		
 		
-def p0_init(nparticles):
+def p0_init(nparticles, pso_init, guess):
 	"""
 	Maximize the Likelihood to start from an optimal position for the MCMC chain
 
@@ -196,6 +197,11 @@ def p0_init(nparticles):
 	
 
 	pso = ParticleSwarmOptimizer(lnprob, low=bnds_min, high=bnds_max, particleCount=nparticles)
+	
+	if pso_init and guess is not None:
+		pso.gbest.position = guess
+		pso.gbest.velocity = [0]*len(guess)
+		pso.gbest.fitness  = lnprob(guess)
 	
 	# sample the parameter space to find the max Likelihood
 	for swarm in pso.sample(maxIter=500):

GS-model/peak_split.py
@@ -29,7 +29,7 @@ def peak_background_bias(nu):
 	
 	
 # ---- Invert the function ----
-nu     = np.linspace(0.,5.,10000)
+nu     = np.linspace(0.,5.,1000)
 f1, f2 = peak_background_bias(nu)
 
 tmp    = (f1 >= -0.2)

GS-model/test.py
@@ -7,24 +7,17 @@ import matplotlib.pyplot as plt
 import numpy as np
 from GS_multi import xi_l, xi_02
 
-r = np.linspace(10,150,16)
+r = np.linspace(10,150,40)
 
-start = time.time()
-xi0 = xi_l(r, 0, 0.98, 1.0, 0.74, 3.5, 0.95, -0.1)
-xi2 = xi_l(r, 2, 0.98, 1.0, 0.74, 3.5, 0.95, -0.1)
-print xi0, xi2
-print time.time() - start
+xi0 = xi_l(r, 0, 1.0, 1.0, 0.82, 2.5, 0.4, -0.55)
+xi2 = xi_l(r, 2, 1.0, 1.0, 0.82, 2.5, 0.4, -0.55)
 
+s, xi0d, err0 = np.loadtxt('data/SGC_bin5_mean_xi0.dat', usecols=(0,1,2), unpack=True)
+xi2d, err2   = np.loadtxt('data/SGC_bin5_mean_xi2.dat', usecols=(1,2),   unpack=True)
 
-sys.exit()
-
-s, xi0d = np.loadtxt('../DR12/Cuesta_2016_CMASSDR12_corrfunction_x0_prerecon.dat', usecols=(0,1,), unpack=True)
-xi2d    = np.loadtxt('../DR12/Cuesta_2016_CMASSDR12_corrfunction_x2_prerecon.dat', usecols=(1,),   unpack=True)
-
-cov = np.loadtxt('../DR12/Cuesta_2016_CMASSDR12_corrfunction_cov_x0x2_prerecon.dat')
-err0 = np.sqrt(np.diag(cov[0:len(s),0:len(s)]))
-err2 = np.sqrt(np.diag(cov[len(s):2*len(s),len(s):2*len(s)]))
-
+#cov = np.loadtxt('data/SGC_bin5_cov_x0x2.dat')
+#err0 = np.sqrt(np.diag(cov[0:len(s),0:len(s)]))
+#err2 = np.sqrt(np.diag(cov[len(s):2*len(s),len(s):2*len(s)]))
 
 plt.figure()
 plt.plot(r,xi0*r*r,'r-',label='model')